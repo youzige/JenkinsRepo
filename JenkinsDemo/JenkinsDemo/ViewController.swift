271a488caebb2c3de5b42443991c228f513be6af
QiCongCong <topqicc@163.com>
2018-07-19 15:45:52 +0800

    ### Add
    ### Modify
    
    ### Delete
    
    ### Fix

---
+++ JenkinsDemo/JenkinsDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JenkinsDemo
+//
+//  Created by Keys on 2018/7/19.
+//  Copyright © 2018年 JuBaoHui Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
